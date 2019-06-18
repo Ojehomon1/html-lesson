dff879b0241d52fab7527c8c85b53af311fef232
Ojehomon1 <periju@web.de>
2019-06-18 10:28:35 +0200

    whileloop

---
+++ javascript/java6/while.js
@@ -0,0 +1,68 @@
+//printing the numbers between 1 and 20 multi 9
+//let i=0;
+//while(i <= 20){
+
+//console.log(i + " * 9 =" + i * 9 );
+//i++;
+//}
+
+//////////////////////////////////////////////////////////////////////
+
+//let j = 20;
+//while (j >= 0){
+
+    //console.log(j + " * 9 =" + j * 9);
+    //j--;
+//}
+
+///////////////////////////////////////////////////////////////////////
+
+//let sum=0,counter=0;
+
+//while(counter <= 10){
+    //sum = sum + counter;
+    //alert( " sum = "+ sum);
+
+    //counter++;
+
+//}
+
+////////////////////////////////////////////////////////////////////
+//let sum=0,counter=0;
+
+//while(counter <= 10){
+    //sum = sum + counter;
+    //alert( "Counter = "+counter+ " sum = "+ sum);
+
+    //counter++;
+
+//}
+
+// let c=prompt("please enter a variable value between 1 and 10")
+// alert(c);
+
+
+////////////////////////////////////////////////////////////////////
+// do while loop
+
+let i=0;
+let str="";
+do{
+    str = "the counter=" + i ;
+    console.log(str);
+    i++;
+
+}
+while(i < 10);
+
+//////////////////////////////////////////////////////////////////////
+
+let t=0;
+let Sum=0;
+do{
+    Sum=Sum+t;
+    console.log("Sum = "+ Sum);
+    t++;
+
+}
+while(t < 10);
