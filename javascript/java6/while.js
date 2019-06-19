b3c07918c2a354e40997a9dfa2bcdcbc50c295bb
Ojehomon1 <periju@web.de>
2019-06-19 14:53:23 +0200

    function

--- javascript/java6/while.js
+++ javascript/java6/while.js
@@ -45,24 +45,117 @@
 ////////////////////////////////////////////////////////////////////
 // do while loop
 
-let i=0;
-let str="";
-do{
-    str = "the counter=" + i ;
-    console.log(str);
-    i++;
+//let i=0;
+//let str="";
+//do{
+    //str = "the counter=" + i ;
+    //console.log(str);
+    //i++;
 
-}
-while(i < 10);
+//}
+//while(i < 10);
 
 //////////////////////////////////////////////////////////////////////
 
-let t=0;
-let Sum=0;
-do{
-    Sum=Sum+t;
-    console.log("Sum = "+ Sum);
-    t++;
+//let t=0;
+//let Sum=0;
+//do{
+   // Sum=Sum+t;
+    //console.log("t ="+t+" and Sum = "+ Sum);
+    //t++;
+
+//}
+//while(t < 10);
+
+//////////////////////////////////////////////////////////////////////
+// Using for-loop to display triangle with stars
+
+// i= 0
+// for (let i= 1 ; i <= 5 ;i++ ) {             // for number of rows
+
+//     for (let c=1 ; c <= 5 ; c++){           // for number of columns
+//      document.write(' * ') 
+//     }
+
+//     document.write(' line number ' + 1 '<br>');
+
+//}
+
+///////////////////////////////////////////////////////////////////////////
+// Using while-loop instead of for-loop to print triange with stars
+//
+
+ i=1;
+
+while(i <= 5){
+    c=1;
+
+    while(c <= 5){
+    document.write('*');
+    c++;
+    }
+
+document.write('<br>');
+i++;
 
 }
-while(t < 10);
+5
+
+/////////////////////////////////////////////////////////////////////////////
+//printing triangle of stars using while loop
+i=1;
+
+while(i <= 5){
+    c=1;
+
+    while(c <= i){
+    document.write('*');
+    c++;
+    }
+
+document.write('<br>');
+i++;
+
+}
+5
+
+///////////////////////////////////////////////////////////////////////////
+//let myList= ["Apple","Orange","Banana","Chicken"];
+//myList.push("Ananas");
+//document.write(myList);
+
+
+
+//////////////////////////////////////////////////////////////////
+
+let veggieFood =["Apple","Orange","Kiwi","Beans","Potato","Banana"]
+veggieFood.push("Chicken","Tomato")
+for(let i=0; i< veggieFood.length; i++){
+
+    if(veggieFood[i]=="Chicken"){
+        alert(" oh nooo your food list is not veggie anymore");
+        break;
+    }
+    console.log(veggieFood[i]);
+}
+///////////////////////////////////////////////////////////////////////////////
+// we delete the none Veggie iten
+
+for(let i=0; i< veggieFood.length; i++){
+
+    if(veggieFood[i]=="Chicken"){
+        alert(" you have chicken and your list is not veggie");
+        veggieFood[i]="Lemon";
+        alert("we changed the chicken to Lemon thank you!!");
+       
+
+    }
+
+    console.log(veggieFood[i]);
+}
+/////////////////////////////////////////////////////////////////U
+// how to delete an item from array list
+let colors=["red","black","green"]
+colors
+delete colors[0];
+color[0]="blue"
---
+++ javascript/java7/funtion.js
@@ -0,0 +1,156 @@
+
+//function dummy(name) {
+    //// do something
+   // alert("Hi "+name);
+
+//}
+
+//dummy("Peter");
+//dummy("Sammy"); dummy("Helen")
+////////////////////////////////////////////////////////
+
+//function greetUser(name, surname) {
+    // do something
+    //alert("Hi "+ name + " " + surname);
+//}
+
+//greetUser("Peter,  Uduevborho")
+
+/////////////////////////////////////////////////////////
+
+//function declaration
+function square(number) {
+    return number*number;
+}
+
+
+//using anonymous function
+var sum = function(number) {
+    return number + 10;
+}
+
+
+function whichDayIsIt(number) {
+    var weekday;
+    //switch goes here: if number is 0 then weekday is "Sunday" etc.....
+    switch (number){
+        case 0:
+            weekday = "Monday";
+            break;
+            
+        case 1:
+            weekday = "Tuesday";
+            break;
+
+
+        case 2:
+            weekday = "Wednesday";
+            break;
+
+        case 3:
+            weekday = "Thursday";
+            break;
+
+        case 4:
+            weekday = "Friday";
+            break;
+
+        case 5:
+            weekday = "Saturday";
+            break;
+
+        case 6:
+            weekday = "Sunday";
+            break;
+         
+        default:
+            alert("Please give a right number.....!");    
+        
+    }
+    return weekday
+}
+
+///////////////////////////////////////////////////////////////////////
+//example - using switch function in for-loop 
+/*
+
+for(let i=0; i<=6, i++) {}
+whichDayIsIt(number),
+
+*/
+/*
+for(let i = 0; i<=6; i++) {
+console.log(whichDayIsIt(i));
+//console.log(i);
+}
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//example - using switch function in while-loop
+let i = 0;
+while(i < 7) {
+
+    console.log("while " + whichDayIsIt (i));
+    i++;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// example - how to create an object
+/* let car = {name: "BMW", year: "2018", color: "black"}
+console.log(car);
+console.log(car.name);
+console.log(car.year);
+console.log(car.color);
+
+
+let car1 = {name: "BMW", color: "black"}
+let car2 = {name: "VW", color: "white"}
+
+let cars = [car1, car2]
+
+console.log(car1, car2);
+*/
+ ///////////////////////////////////////////////////////////////////////
+ //please write a function to place country depending on name of the care
+
+ let car1 = {name: "BMW", country: "Germany"};
+ let car2 = {name: "Ford", country: "USA"};
+ let car3 = {name: "Fiat", country: "Italy"};
+
+ let cars = [car1, car2, car3];
+ 
+ function findCarCountry(name) {
+     let country;
+     for(let i=0; i<cars.length; i++) {
+    
+        if(cars[i].name == name){
+        country = cars[i].country;  
+        }
+
+     }
+
+         return country;
+
+ }
+    console.log(findCarCountry("Fiat"));
+
+///////////////////////////////////////////////////////////////////////
+
+    let newCars = ["BMW", "Ford", "Ferrari"];
+    function findYourCar(car) {
+        let found = false;
+        for (let i = 0; i < newCars.length; i++) {
+            if (newCars[i] == car) {
+               found = true;
+            
+            }
+         
+            }
+            if (found) {
+                console.log("Found");
+        }
+        else {
+            console.log("Not Found");
+        }
+    }
+    console.log(findYourCar("Passat"));
