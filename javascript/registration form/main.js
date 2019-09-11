e785fb69d003307b34fd19f04a866eaac0198feb
Ojehomon1 <periju@web.de>
2019-09-11 12:15:21 +0200

    babel

--- javascript/registration form/main.js
+++ javascript/registration form/main.js
@@ -2,46 +2,109 @@ document.getElementById("personalDiv").style.display="block";
 document.getElementById("addressDiv").style.display="none";
 document.getElementById("profileInfo").style.display="none";
 document.getElementById("resultDiv").style.display="none";
-​
-​
-​
+
+
+
 function nextDiv(e){
     e.preventDefault();
-    //alert("Next btn is clicked")
+    let checkFirstName = document.getElementsByName("firstName")[0].checkValidity();
+    let checkLastName = document.getElementsByName("lastName")[0].checkValidity();
+    let checkBirthDate = document.getElementsByName("birthDate")[0].checkValidity();
+    let checkNationality = document.getElementsByName("nationality")[0].checkValidity();
+
+    let checkAddress=document.getElementsByName("address")[0].checkValidity();
+    let checkHausNumber=document.getElementsByName("houseNumber")[0].checkValidity();
+    let checkZipeCode = document.getElementsByName("zipeCode")[0].checkValidity();
+    
+    let checkPassword = document.getElementsByName("password")[0].checkValidity();    
+    let passwordMatch = 
+    document.getElementsByName('password')[0].value ===
+    document.getElementsByName('passwordRepeat')[0].value;
+    //alert(passwordMatch);
+
+    
+
+
+ document.getElementById('photoLabel').src = imageFile.replace('C:\\fakepath\\', './images/' );
+
+  
+
     let parentDivID = e.target.parentElement.id;
     //alert(parentDivID);
     switch(parentDivID){
         case 'personalDiv':
-            e.target.parentElement.style.display="none";
-            document.getElementById("addressDiv").style.display="block";
-            document.getElementById("profileInfo").style.display="none";
-            document.getElementById("resultDiv").style.display="none";
+            if(checkFirstName && checkLastName && checkBirthDate && checkNationality ){
+
+                e.target.parentElement.style.display="none";
+                document.getElementById("addressDiv").style.display="block";
+                document.getElementById("profileInfo").style.display="none";
+                document.getElementById("resultDiv").style.display="none";
+
+            }
+            else{  alert('please check your entries');   }
+
+            
             break;
         case 'addressDiv':
-            e.target.parentElement.style.display="none";
-            document.getElementById("resultDiv").style.display="none";
-            document.getElementById("personalDiv").style.display="none";
-            document.getElementById("profileInfo").style.display="block";
+            if(checkAddress && checkHausNumber && checkZipeCode  ){
+                e.target.parentElement.style.display="none";
+                document.getElementById("resultDiv").style.display="none";
+                document.getElementById("personalDiv").style.display="none";
+                document.getElementById("profileInfo").style.display="block";
+            }
+            else {
+                alert('please check your entries');
+            }
+           
             break;
         case 'profileInfo' :
             e.target.parentElement.style.display="none";
             document.getElementById("addressDiv").style.display="none";
             document.getElementById("personalDiv").style.display="none";
             document.getElementById("resultDiv").style.display="block";
-​
-​
-​
-​
-​
-​
+
     }
    
-​
-​
-​
+
+
+
 }
-function previousDiv(){
-    alert("Previous button is clicked");
+function previousDiv(e){
+    e.preventDefault();
+     let parentDiv = e.target.parentElement.id;
+     //alert(parentDiv);
+     switch(parentDiv){
+         case'addressDiv' :
+            e.target.parentElement.style.display="none";
+            document.getElementById("personalDiv").style.display="block";
+            document.getElementById("resultDiv").style.display="none";
+            document.getElementById("profileInfo").style.display="none";
+            break;
+          case 'profileInfo' : 
+            e.target.parentElement.style.display="none";
+            document.getElementById("addressDiv").style.display="block";
+            document.getElementById("resultDiv").style.display="none";
+            document.getElementById("personalDiv").style.display="none";
+            break;
+         case  'resultDiv' : 
+            e.target.parentElement.style.display="none";
+            document.getElementById("profileInfo").style.display="block";
+            document.getElementById("addressDiv").style.display="none";
+            document.getElementById("personalDiv").style.display="none";
+            break;
+
+
+
+
+
+
+                  
+
+
+
+
+     }
+    
 }
 let btns = document.querySelectorAll("button");
 console.log(btns);
@@ -52,9 +115,7 @@ btns.forEach(function(item){
   }
   else if (item.classList.contains('previous')){
     item.addEventListener("click",previousDiv);
-​
+
   }
-​
+
 });
-
-
